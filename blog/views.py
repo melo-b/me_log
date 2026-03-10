b35eb7a3430f16d4a82b9f1bf36f19e3c17b5456
melo-b <rommelo.b@gmail.com>
2026-03-10 11:50:01 -0700

    Add comment form, update detail view to handle POST request, and display comments

--- blog/views.py
+++ blog/views.py
@@ -1,5 +1,6 @@
-from django.shortcuts import render, get_object_or_404
-from .models import Post
+from django.shortcuts import render, get_object_or_404, redirect
+from .models import Post, Comment
+from .forms import CommentForm
 
 # Create your views here.
 
@@ -17,7 +18,24 @@ def post_detail(request, pk):
     # This securely fetches the post by its ID (primary key) or returns a 404 error if not found
     post = get_object_or_404(Post, pk=pk)
     
+    # Fetch all comments linked to this specfic post
+    comments = Comment.objects.filter(post=post).order_by('created_on')
+    
+    # Handle form submission
+    if request.method == 'POST':
+        form = CommentForm(request.POST)
+        if form.is_valid():
+            comment = form.save(commit=False)  # Create a Comment object but don't save to the database yet
+            comment.post = post # Link the comment to the current post
+            comment.save() # Now save the comment to the database
+            return redirect('post_detail', pk=post.pk) # Redirect to the same post detail page after saving the comment
+        
+    else:
+            form = CommentForm() # If the form is not valid, create a new empty form
+    
     context = {
-        'post': post
+        'post': post,
+        'comments': comments,
+        'form': form
         }
     return render(request, 'blog/post_detail.html', context)
